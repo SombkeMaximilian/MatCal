e03533874425976a313c62ecabb10d504d587809
Sombke Maximilian <Maximilian.Sombke@stud.uni-regensburg.de>
2024-02-21 12:12:17 +0100

    Added start of operator test program

---
+++ test/matrix_operator_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <complex>
+#include <vector>
+#include "matrix.hpp"
+#include "test_utils.hpp"
+
+template<typename T>
+class MatrixOperators : public ::testing::Test {};
