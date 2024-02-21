fa87a3dd41f9890b904febf1fc290c1668453d5d
Sombke Maximilian <Maximilian.Sombke@stud.uni-regensburg.de>
2024-02-21 11:41:59 +0100

    Changed double to TypeParam for typed test

--- test/matrix_element_access_test.cpp
+++ test/matrix_element_access_test.cpp
@@ -8,11 +8,11 @@ class MatrixElementAccess : public ::testing::Test {};
 
 TYPED_TEST_SUITE(MatrixElementAccess, MatrixTypes);
 
-TEST(MatrixAccess, AccessElementOutOfRange) {
+TYPED_TEST(MatrixElementAccess, AccessElementOutOfRange) {
     size_t init_dim{3};
-    linalg::Matrix<double> m(init_dim);
+    linalg::Matrix<TypeParam> m(init_dim);
 
-    EXPECT_THROW({ double val{m(3, 0)}; }, std::out_of_range);
-    EXPECT_THROW({ double val{m(0, 3)}; }, std::out_of_range);
-    EXPECT_THROW({ double val{m(3, 3)}; }, std::out_of_range);
+    EXPECT_THROW({ TypeParam val{m(3, 0)}; }, std::out_of_range);
+    EXPECT_THROW({ TypeParam val{m(0, 3)}; }, std::out_of_range);
+    EXPECT_THROW({ TypeParam val{m(3, 3)}; }, std::out_of_range);
 }
