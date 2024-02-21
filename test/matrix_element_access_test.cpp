4dbf1f6fe104d71dbe97da01355bf36b04d46414
Sombke Maximilian <Maximilian.Sombke@stud.uni-regensburg.de>
2024-02-21 14:47:27 +0100

    Split matrix access test into 3 parts

--- test/matrix_element_access_test.cpp
+++ test/matrix_element_access_test.cpp
@@ -8,11 +8,23 @@ class MatrixElementAccess : public ::testing::Test {};
 
 TYPED_TEST_SUITE(MatrixElementAccess, MatrixTypes);
 
-TYPED_TEST(MatrixElementAccess, AccessElementOutOfRange) {
+TYPED_TEST(MatrixElementAccess, AccessElementOutOfRangeRow) {
     size_t init_dim{3};
     linalg::Matrix<TypeParam> m(init_dim);
 
     EXPECT_THROW({ TypeParam val{m(3, 0)}; }, std::out_of_range);
+}
+
+TYPED_TEST(MatrixElementAccess, AccessElementOutOfRangeCol) {
+    size_t init_dim{3};
+    linalg::Matrix<TypeParam> m(init_dim);
+
     EXPECT_THROW({ TypeParam val{m(0, 3)}; }, std::out_of_range);
+}
+
+TYPED_TEST(MatrixElementAccess, AccessElementOutOfRangeRowCol) {
+    size_t init_dim{3};
+    linalg::Matrix<TypeParam> m(init_dim);
+
     EXPECT_THROW({ TypeParam val{m(3, 3)}; }, std::out_of_range);
 }
