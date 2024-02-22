676713743518c079d515fd3083b4c3d09094ba9b
Sombke Maximilian <Maximilian.Sombke@stud.uni-regensburg.de>
2024-02-22 15:36:20 +0100

    Refactored some variable names
    - consistently use snake case instead of camel case for names
    - changed one letter names to something more meaningful

--- test/matrix_operator_test.cpp
+++ test/matrix_operator_test.cpp
@@ -14,15 +14,15 @@ TYPED_TEST(MatrixOperators, PlusOperator) {
     std::vector<TypeParam> init_vec1{1, 2, 3, 4};
     std::vector<TypeParam> init_vec2{5, 6, 7, 8};
     std::vector<TypeParam> expected_vec{6, 8, 10, 12};
-    linalg::Matrix<TypeParam> m1(init_dim, init_vec1);
-    linalg::Matrix<TypeParam> m2(init_dim, init_vec2);
-    linalg::Matrix<TypeParam> resultMatrix(init_dim);
-    linalg::Matrix<TypeParam> expectedMatrix(init_dim, expected_vec);
+    linalg::Matrix<TypeParam> test_matrix1(init_dim, init_vec1);
+    linalg::Matrix<TypeParam> test_matrix2(init_dim, init_vec2);
+    linalg::Matrix<TypeParam> result_matrix;
+    linalg::Matrix<TypeParam> expected_matrix(init_dim, expected_vec);
 
-    resultMatrix = m1 + m2;
+    result_matrix = test_matrix1 + test_matrix2;
     for ( size_t i = 0; i < init_dim; ++i ) {
         for ( size_t j = 0; j < init_dim; ++j ) {
-            expect_type_eq(resultMatrix(i, j), expectedMatrix(i, j));
+            expect_type_eq(result_matrix(i, j), expected_matrix(i, j));
         }
     }
 }
@@ -32,15 +32,15 @@ TYPED_TEST(MatrixOperators, MinusOperator) {
     std::vector<TypeParam> init_vec1{1, 8, 2, 9};
     std::vector<TypeParam> init_vec2{3, 6, 4, 7};
     std::vector<TypeParam> expected_vec{-2, 2, -2, 2};
-    linalg::Matrix<TypeParam> m1(init_dim, init_vec1);
-    linalg::Matrix<TypeParam> m2(init_dim, init_vec2);
-    linalg::Matrix<TypeParam> resultMatrix(init_dim);
-    linalg::Matrix<TypeParam> expectedMatrix(init_dim, expected_vec);
+    linalg::Matrix<TypeParam> test_matrix1(init_dim, init_vec1);
+    linalg::Matrix<TypeParam> test_matrix2(init_dim, init_vec2);
+    linalg::Matrix<TypeParam> result_matrix;
+    linalg::Matrix<TypeParam> expected_matrix(init_dim, expected_vec);
 
-    resultMatrix = m1 - m2;
+    result_matrix = test_matrix1 - test_matrix2;
     for ( size_t i = 0; i < init_dim; ++i ) {
         for ( size_t j = 0; j < init_dim; ++j ) {
-            expect_type_eq(resultMatrix(i, j), expectedMatrix(i, j));
+            expect_type_eq(result_matrix(i, j), expected_matrix(i, j));
         }
     }
 }
@@ -49,14 +49,14 @@ TYPED_TEST(MatrixOperators, UnaryMinusOperator) {
     const size_t init_dim{2};
     std::vector<TypeParam> init_vec{1, 2, 3, 4};
     std::vector<TypeParam> expected_vec{-1, -2, -3, -4};
-    linalg::Matrix<TypeParam> m1(init_dim, init_vec);
-    linalg::Matrix<TypeParam> m2(init_dim);
-    linalg::Matrix<TypeParam> expectedMatrix(init_dim, expected_vec);
+    linalg::Matrix<TypeParam> test_matrix(init_dim, init_vec);
+    linalg::Matrix<TypeParam> result_matrix;
+    linalg::Matrix<TypeParam> expected_matrix(init_dim, expected_vec);
 
-    m2 = -m1;
+    result_matrix = -test_matrix;
     for ( size_t i = 0; i < init_dim; ++i ) {
         for ( size_t j = 0; j < init_dim; ++j ) {
-            expect_type_eq(m2(i, j), expectedMatrix(i, j));
+            expect_type_eq(result_matrix(i, j), expected_matrix(i, j));
         }
     }
 }
