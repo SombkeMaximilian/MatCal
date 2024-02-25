185d03e729ad4ccaca66580fcd293681d48c8d34
Sombke Maximilian <Maximilian.Sombke@stud.uni-regensburg.de>
2024-02-25 02:55:00 +0100

    Simplified test

--- test/matrix_element_access_test.cpp
+++ test/matrix_element_access_test.cpp
@@ -4,27 +4,23 @@
 #include "test_utils.hpp"
 
 template<typename T>
-class MatrixElementAccess : public ::testing::Test {};
+class MatrixElementAccess : public ::testing::Test {
+protected:
+    size_t init_dim{3};
+    linalg::Matrix<T> test_matrix;
+    void SetUp() override {
+        test_matrix = linalg::Matrix<T>(init_dim);
+    }
+};
 
 TYPED_TEST_SUITE(MatrixElementAccess, MatrixTypes);
 
 TYPED_TEST(MatrixElementAccess, AccessElementOutOfRangeRow) {
-    size_t init_dim{3};
-    linalg::Matrix<TypeParam> test_matrix(init_dim);
-
-    EXPECT_THROW({ TypeParam val{test_matrix(3, 0)}; }, std::out_of_range);
+    EXPECT_THROW({ TypeParam val{this->test_matrix(3, 0)}; }, std::out_of_range);
 }
-
 TYPED_TEST(MatrixElementAccess, AccessElementOutOfRangeCol) {
-    size_t init_dim{3};
-    linalg::Matrix<TypeParam> test_matrix(init_dim);
-
-    EXPECT_THROW({ TypeParam val{test_matrix(0, 3)}; }, std::out_of_range);
+    EXPECT_THROW({ TypeParam val{this->test_matrix(0, 3)}; }, std::out_of_range);
 }
-
 TYPED_TEST(MatrixElementAccess, AccessElementOutOfRangeRowCol) {
-    size_t init_dim{3};
-    linalg::Matrix<TypeParam> test_matrix(init_dim);
-
-    EXPECT_THROW({ TypeParam val{test_matrix(3, 3)}; }, std::out_of_range);
+    EXPECT_THROW({ TypeParam val{this->test_matrix(3, 3)}; }, std::out_of_range);
 }
