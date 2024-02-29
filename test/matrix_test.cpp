ceaee95d8e16d8f7f5c94a4c11f8552f97588738
Sombke Maximilian <Maximilian.Sombke@stud.uni-regensburg.de>
2024-02-29 08:21:51 +0100

    Moved part of test_utils to matrix_test

--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -1,3 +1,13 @@
 #include <gtest/gtest.h>
 #include "matrix.hpp"
 #include "test_utils.hpp"
+
+using MatrixTypes = ::testing::Types<
+        int,
+        short,
+        long,
+        float,
+        double,
+        std::complex<float>,
+        std::complex<double>
+>;
--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -1,46 +1,7 @@
 #ifndef MATCAL_TEST_UTILS_HPP
 #define MATCAL_TEST_UTILS_HPP
 
-#include <vector>
 #include <complex>
-#include <random>
-#include <type_traits>
-
-using MatrixTypes = ::testing::Types<
-        int,
-        short,
-        long,
-        float,
-        double,
-        std::complex<float>,
-        std::complex<double>
->;
-
-template<typename T>
-std::vector<T> generateRandomVector(size_t n) {
-    std::vector<T> vec(n);
-    std::mt19937 rng(42);
-    if constexpr (std::is_integral<T>::value) {
-        std::uniform_int_distribution<T> dist(0, 100);
-        for (size_t i = 0; i < n; ++i) {
-            vec[i] = dist(rng);
-        }
-    } else if constexpr (std::is_floating_point<T>::value) {
-        std::uniform_real_distribution<T> dist(0.0, 1.0);
-        for (size_t i = 0; i < n; ++i) {
-            vec[i] = dist(rng);
-        }
-    } else if constexpr (std::is_same<T, std::complex<float>>::value || std::is_same<T, std::complex<double>>::value) {
-        using value_type = typename T::value_type;
-        std::uniform_real_distribution<value_type> dist(0.0, 1.0);
-        for (size_t i = 0; i < n; ++i) {
-            value_type realPart = dist(rng);
-            value_type imagPart = dist(rng);
-            vec[i] = T(realPart, imagPart);
-        }
-    }
-    return vec;
-}
 
 template<typename T>
 void expect_type_eq(const T& actual, const T& expected) {
