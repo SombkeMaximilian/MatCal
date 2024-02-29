0fda2a242ff339ebd1e373375051374f2b854f6d
Sombke Maximilian <Maximilian.Sombke@stud.uni-regensburg.de>
2024-02-29 11:32:42 +0100

    Moved and renamed operator tests

--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -146,3 +146,71 @@ TYPED_TEST(MatrixElements, AccessElementOutOfRangeRowCol) {
         },
         std::out_of_range);
 }
+
+
+template<typename T>
+class MatrixArithmetics : public ::testing::Test {
+
+protected:
+
+    void testMatrixDimensions(linalg::Matrix<T>& m, size_t expected_rows, size_t expected_cols) {
+        EXPECT_EQ(m.getRows(), expected_rows);
+        EXPECT_EQ(m.getCols(), expected_cols);
+    }
+
+    void testMatrixElements(linalg::Matrix<T>& m, std::vector<T>& expected_values) {
+        for ( size_t i = 0; i < m.getRows(); ++i ) {
+            for ( size_t j = 0; j < m.getCols(); ++j ) {
+                EXPECT_TYPE_EQ(m(i, j), expected_values[i * m.getCols() + j]);
+            }
+        }
+    }
+
+}; // Matrix Arithmetics
+
+TYPED_TEST_SUITE(MatrixArithmetics, MatrixTypes);
+
+TYPED_TEST(MatrixArithmetics, PlusOperator) {
+    const size_t init_dim{2};
+    std::vector<TypeParam> init_vec1{1, 2, 3, 4};
+    std::vector<TypeParam> init_vec2{5, 6, 7, 8};
+    std::vector<TypeParam> expected_vec{6, 8, 10, 12};
+    linalg::Matrix<TypeParam> test_matrix1(init_dim, init_vec1);
+    linalg::Matrix<TypeParam> test_matrix2(init_dim, init_vec2);
+    linalg::Matrix<TypeParam> result_matrix;
+    result_matrix = test_matrix1 + test_matrix2;
+    this->testMatrixElements(result_matrix, expected_vec);
+}
+TYPED_TEST(MatrixArithmetics, MinusOperator) {
+    const size_t init_dim{2};
+    std::vector<TypeParam> init_vec1{1, 8, 2, 9};
+    std::vector<TypeParam> init_vec2{3, 6, 4, 7};
+    std::vector<TypeParam> expected_vec{-2, 2, -2, 2};
+    linalg::Matrix<TypeParam> test_matrix1(init_dim, init_vec1);
+    linalg::Matrix<TypeParam> test_matrix2(init_dim, init_vec2);
+    linalg::Matrix<TypeParam> result_matrix;
+    result_matrix = test_matrix1 - test_matrix2;
+    this->testMatrixElements(result_matrix, expected_vec);
+}
+TYPED_TEST(MatrixArithmetics, UnaryMinusOperator) {
+    const size_t init_dim{2};
+    std::vector<TypeParam> init_vec{1, 2, 3, 4};
+    std::vector<TypeParam> expected_vec{-1, -2, -3, -4};
+    linalg::Matrix<TypeParam> test_matrix(init_dim, init_vec);
+    linalg::Matrix<TypeParam> result_matrix;
+    linalg::Matrix<TypeParam> expected_matrix(init_dim, expected_vec);
+    result_matrix = -test_matrix;
+    this->testMatrixElements(result_matrix, expected_vec);
+}
+TYPED_TEST(MatrixArithmetics, MultiplicationOperator) {
+    const size_t init_row1{2}, init_row2{3}, init_col1{3}, init_col2{2}, expected_dim{2};
+    std::vector<TypeParam> init_vec1{1, 2, 3, 4, 5, 6};
+    std::vector<TypeParam> init_vec2{6, 5, 4, 3, 2, 1};
+    std::vector<TypeParam> expected_vec{20, 14, 56, 41};
+    linalg::Matrix<TypeParam> test_matrix1(init_row1, init_col1, init_vec1);
+    linalg::Matrix<TypeParam> test_matrix2(init_row2, init_col2, init_vec2);
+    linalg::Matrix<TypeParam> result_matrix;
+    result_matrix = test_matrix1 * test_matrix2;
+    this->testMatrixDimensions(result_matrix, expected_dim, expected_dim);
+    this->testMatrixElements(result_matrix, expected_vec);
+}
